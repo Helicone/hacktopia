0aceaad736fd4636ce94c9315a08c849a306a7da
Justin <justintorre75@gmail.com>
2023-11-07 19:41:46 -0800

    first rev

---
+++ src/app/api/emails/route.ts
@@ -0,0 +1,29 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const json = await request.json();
+  console.log("JSON", json);
+  try {
+    const result = await sql`
+      INSERT INTO emails (email, first_name, last_name, company, title)
+      VALUES (${json.email}, ${json.first_name}, ${json.last_name}, ${json.company}, ${json.title})
+      ON CONFLICT (email) DO NOTHING
+      RETURNING *`;
+    // Check if a row was inserted
+    if (result.rowCount === 0) {
+      // No rows inserted, meaning there was a duplicate email
+      return NextResponse.json(
+        { error: "Email already exists" },
+        { status: 409 }
+      );
+    }
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,181 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
 
-export default function Home() {
+export default function WaitlistPage() {
+  const [formData, setFormData] = useState({
+    email: "",
+    first_name: "",
+    last_name: "",
+    company: "",
+    title: "",
+  });
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/emails", {
+        method: "POST",
+        body: JSON.stringify({
+          email: formData.email,
+          first_name: formData.first_name,
+          last_name: formData.last_name,
+          ...(formData.company && { company: formData.company }),
+          ...(formData.title && { title: formData.title }),
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setMessage(data.message);
+      setError(data.error);
+    } catch (error) {
+      setMessage("An error occurred. Please try again later.");
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="container mx-auto p-4 justify-center flex flex-col lg:h-screen">
+      <h1 className="text-7xl  mb-4 font-mono h-14 mx-auto hidden lg:block">
+        Hacktopia 2023
+      </h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="lg:flex flex-col lg:flex-row justify-center mx-auto mb-4 lg:gap-6 hidden">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://posthog.com/brand/posthog-logo-black.svg"
+          alt="PostHog Logo"
+          className="h-12 "
+        />
+        <div className="text-3xl font-bold my-4 mx-auto">+</div>
+
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://raw.githubusercontent.com/Helicone/helicone/main/docs/logo/light.svg"
+          alt="Helicone Logo"
+          className="h-12 "
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+      {/* R=242, G=235, B=219*/}
+      <div
+        className="flex flex-col lg:flex-row justify-center rounded-lg shadow-lg p-4 max-w-5xl mx-auto"
+        style={{ backgroundColor: "#F2EBDB" }}
+      >
+        <div className="flex flex-col align-middle justify-center mx-12 max-w-sm">
+          <p className="text-xl mx-auto mb-6 text-center font-light tracking-wide leading-relaxed">
+            Get ready for the ultimate coding experience at{" "}
+            <span className="font-bold">Hacktopia</span>, the place where
+            developers and tech enthusiasts collaborate to transform their big
+            ideas into reality!
+            <span className="block font-bold mt-2">
+              {" "}
+              Date: December 2nd, 2023{" "}
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+            <span className="block font-bold">
+              {" "}
+              Location: The Mission, San Francisco{" "}
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
-        </a>
+        </div>
+        <div className="flex flex-col justify-center w-full">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="/hacktopia.png"
+            alt="Hacktopia"
+            className="h-96 w-96 mx-auto"
+          />
+          <div>
+            <form
+              className="space-y-4 max-w-lg mx-auto"
+              onSubmit={handleSubmit}
+            >
+              <div
+                className="grid grid-cols-2 gap-4"
+                style={{ gridTemplateColumns: "1fr 1fr" }}
+              >
+                <input
+                  type="text"
+                  name="first_name"
+                  placeholder="First name"
+                  value={formData.first_name}
+                  onChange={handleChange}
+                  required
+                  className="w-full border border-gray-300 dark:border-neutral-700 rounded-lg p-2 dark:text-white dark:bg-gray-800"
+                />
+                <input
+                  type="text"
+                  name="last_name"
+                  placeholder="Last name"
+                  value={formData.last_name}
+                  onChange={handleChange}
+                  required
+                  className="w-full border border-gray-300 dark:border-neutral-700 rounded-lg p-2 dark:text-white dark:bg-gray-800"
+                />
+              </div>
+              <div
+                className="grid grid-cols-2 gap-4"
+                style={{ gridTemplateColumns: "1fr 1fr" }}
+              >
+                <input
+                  type="text"
+                  name="company"
+                  placeholder="Company (Optional)"
+                  value={formData.company}
+                  onChange={handleChange}
+                  className="w-full border border-gray-300 dark:border-neutral-700 rounded-lg p-2 dark:text-white dark:bg-gray-800"
+                />
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Title (Optional)"
+                  value={formData.title}
+                  onChange={handleChange}
+                  className="w-full border border-gray-300 dark:border-neutral-700 rounded-lg p-2 dark:text-white dark:bg-gray-800"
+                />
+              </div>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className="w-full border border-gray-300 dark:border-neutral-700 rounded-lg p-2 dark:text-white dark:bg-gray-800"
+              />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+              <button
+                type="submit"
+                className="w-full bg-gray-900 text-white rounded-lg p-2"
+              >
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-    </main>
-  )
+      <div className="flex flex-col lg:flex-row justify-center mx-auto mt-4 lg:gap-6 lg:hidden">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://posthog.com/brand/posthog-logo-black.svg"
+          alt="PostHog Logo"
+          className="h-12 "
+        />
+        <div className="text-3xl font-bold my-4 mx-auto">+</div>
+
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://raw.githubusercontent.com/Helicone/helicone/main/docs/logo/light.svg"
+          alt="Helicone Logo"
+          className="h-12 "
+        />
+      </div>
+    </div>
+  );
 }
